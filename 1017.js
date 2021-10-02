e5003576debb849aa0d8469565e0adccdc62fab3
Jorge Clênio <cleniocontato@gmail.com>
2021-10-02 16:09:57 -0300

    Troca de pasta

--- 1017.js
+++
@@ -1,7 +0,0 @@
-// https://www.urionlinejudge.com.br/judge/pt/problems/view/1017
-var input = require('fs').readFileSync('stdin', 'utf8');
-const [tGasto, vMedia] = input.split("\n");
-
-const tPercorrido = (tGasto * vMedia) / 12;
-
-console.log(tPercorrido.toFixed(3));
--- 1035.js
+++
@@ -1,9 +0,0 @@
-// https://www.urionlinejudge.com.br/judge/pt/problems/view/1035
-const input = require('fs').readFileSync('stdin', 'utf8');
-const [A, B, C, D] = input.split(" ");
-
-if (B > C && D > A && C + D > A + B && C > 0 && D > 0 && A % 2 == 0) {
-    console.log("Valores aceitos");
-} else {
-    console.log("Valores nao aceitos");
-}
